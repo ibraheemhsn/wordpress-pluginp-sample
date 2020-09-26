75f0a5c5c624e7938a2ebbffbec77829314f8cc9
ibraheemhsn <ibraheemhsn@users.noreply.github.com>
2020-09-26 13:01:41 +0300

    Add files via upload

---
+++ ib-custom-functions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: ib Custom Functions
+Plugin URI: http://wordimpress.com
+Description: Custom functions for wordpress
+Author: Ibrahim
+Version: 1.2
+Requires at least: 3.8.1
+Author URI: http://imdev.in
+*/
+
